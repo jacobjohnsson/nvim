c82c3dcf9be63924b63acaac481f5e00f9c81844
Jacob Johnsson <jacobj@axis.com>
2025-02-28 08:57:19 +0100

    Clean up old lsp.
    Change-Id: I1813ce9f4efd9d1a0f3adb5a8a7c254f96c730cb

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -180,15 +180,6 @@ require("lazy").setup({
             vim.api.nvim_set_hl(0, "BlinkCmpGhostText", { link = "Grey" })
         end,
     },
-    -- {
-    --     "hrsh7th/cmp-nvim-lsp",
-    --     config = function()
-    --         require('cmp_nvim_lsp').default_capabilities()
-    --     end,
-    -- },
-    -- { "hrsh7th/cmp-buffer" },
-    -- { "hrsh7th/cmp-path" },
-    -- { "hrsh7th/cmp-cmdline" },
     {
         'saghen/blink.cmp',
         dependencies = 'rafamadriz/friendly-snippets',
@@ -216,52 +207,4 @@ require("lazy").setup({
             vim.api.nvim_set_hl(0, "BlinkCmpGhostText", { link = "Grey" })
         end,
     },
-    -- {
-    --     "hrsh7th/nvim-cmp",
-    --     config = function(args)
-    --         local cmp = require('cmp')
-    --         cmp.setup({
-    --             snippet = {
-    --                 -- Use the built in neovim snippet engine.
-    --                 -- vim.snippet.expand(args.body)
-    --             },
-    --             window = {
-    --                 -- completion = cmp.config.window.bordered(),
-    --                 documentation = cmp.config.window.bordered(),
-    --             },
-    --             mapping = cmp.mapping.preset.insert({
-    --                   ['<C-d>'] = cmp.mapping.open_docs(),
-    --                   ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    --                   ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    --                   ['<C-Space>'] = cmp.mapping.complete(),
-    --                   ['<C-e>'] = cmp.mapping.abort(),
-    --                   ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    --             }),
-    --             sources = cmp.config.sources({
-    --                 { name = 'nvim_lsp' },
-    --                 -- { name = 'vsnip' }, -- For vsnip users.
-    --                 -- { name = 'luasnip' }, -- For luasnip users.
-    --                 -- { name = 'ultisnips' }, -- For ultisnips users.
-    --                 -- { name = 'snippy' }, -- For snippy users.
-    --             }, {
-    --               { name = 'buffer' },
-    --             })
-    --         })
-    --         cmp.setup.cmdline({ '/', '?' }, {
-    --             mapping = cmp.mapping.preset.cmdline(),
-    --             sources = {
-    --                 { name = 'buffer' }
-    --             }
-    --         })
-    --         cmp.setup.cmdline(':', {
-    --             mapping = cmp.mapping.preset.cmdline(),
-    --             sources = cmp.config.sources({
-    --                 { name = 'path' }
-    --             }, {
-    --                 { name = 'cmdline' }
-    --             }),
-    --             matching = { disallow_symbol_nonprefix_matching = false }
-    --         })
-    --     end,
-    -- },
 })
